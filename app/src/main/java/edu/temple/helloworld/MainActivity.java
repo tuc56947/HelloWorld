13e3510101d11710ebc1c8604199d027ccf87d99
David Lee <tuc56947@temple.edu>
2021-02-07 14:40:03 -0500

    Added Button Added Button and onClick activity where textView text changes when button clicked

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,27 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        final TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                textView.setText("You have clicked the button");
+            }
+        });
+
     }
 }
